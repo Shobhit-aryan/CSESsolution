6c0a4cc93f081e7e24fae485f4e457d22506c484
Shobhit Aryan Lal <55244666+Shobhit-aryan@users.noreply.github.com>
2021-03-13 23:54:46 +0530

    Add files via upload

---
+++ array description.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+
+
+int solve(vector<int>& v,int n, int m){
+	int dp[n+2][m+2];
+	memset(dp, 0, sizeof dp);
+	
+	for(int i=1;i<=n;i++){
+		for(int x=1;x<=m;x++){
+			if(i==1){
+				if(v[i]==0 || v[i]==x){
+					dp[i][x] = 1;	
+				}
+				else{
+					dp[i][x] = 0;
+				}
+			}
+			else{
+				if(v[i]==0 || v[i] == x){
+					dp[i][x] = ((dp[i-1][x-1]+dp[i-1][x])%mod+dp[i-1][x+1])%mod;
+				}
+				else
+					dp[i][x] = 0;
+			}
+		}	
+	}
+	int ans = 0;
+	for(int x=1;x<=m;x++){
+		ans = (ans+ dp[n][x])%mod;
+	}
+	return ans;
+}
+
+int main(){
+	int n,m;
+	cin>>n>>m;
+	vector<int> v(n+1);
+	for(int i=1;i<=n;i++)
+	cin>>v[i];
+	
+	cout<<solve(v,n,m);
+}
